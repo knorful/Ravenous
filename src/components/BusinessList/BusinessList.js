b1c512789709e3c3d74a85db22470d21fb79327d
Kris Norful <krisnorful@gmail.com>
2020-09-02 18:30:05 -0500

    Add more functionality

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,14 +4,13 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
   render() {
-    <div className="BusinessList">
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-    </div>
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => <Business business={business} />)
+        }
+      </div>
+    )
   }
 }
 
